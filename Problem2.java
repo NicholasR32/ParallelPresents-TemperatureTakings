e61efd56cd097b15483a6ef7c7e972be06cfb511
NicholasR32 <98115878+NicholasR32@users.noreply.github.com>
2024-04-04 23:48:15 -0400

    Finish problem 2

--- Problem2.java
+++ Problem2.java
@@ -1,3 +1,7 @@
+// Nicholas Rolland
+// Assignment 3
+// COP 4520, Spring 2024, Prof. Juan Parra
+
 // Problem 2: Atmospheric Temperature Reading Module
 // On the Mars Rover are 8 temperature sensors, each with its own CPU
 // Each sensor takes a reading every 1 minute (between -100F and 70F)
@@ -18,24 +22,26 @@ public class Problem2
     public static final int NUM_MINUTES = 60;
     public static final int INTERVAL = 10;
 
+    public static final boolean DEBUG = false;
     public static void main(String[] args)
     {
-        // Create threads
-        Sensor[] sensors = new Sensor[8];
-        for (int i = 0; i < NUM_SENSORS; i++)
-        {
-            sensors[i] = new Sensor(i);
-        }
-
         // Start temp readings
         for (int h = 0; h < NUM_HOURS; h++)
         {
-            int[][] hourlyReadings = new int[NUM_MINUTES][NUM_SENSORS];
+            double[][] hourlyReadings = new double[NUM_MINUTES][NUM_SENSORS];
             for (int m = 0; m < NUM_MINUTES; m++)
             {
+                // Create threads
+                Sensor[] sensors = new Sensor[8];
+                for (int i = 0; i < NUM_SENSORS; i++)
+                {
+                    sensors[i] = new Sensor(i);
+                }
                 // Get readings from all sensors for this minute.
                 for (Sensor sensor : sensors)
+                {
                     sensor.start();
+                }
                 for (Sensor sensor : sensors)
                 {
                     try
@@ -54,23 +60,23 @@ public class Problem2
             }
 
             // Compile report at end of each hour.
-            int maxTempDiff = 0;
+            double maxTempDiff = 0;
             int maxStart = 0, maxEnd = 0;
             int start = 0, end = 9;
             while (end < NUM_MINUTES)
             {
                 // Note highest and lowest temp recorded by any sensor in this interval.
-                int minTemp = 999;
-                int maxTemp = -999;
+                double minTemp = 999;
+                double maxTemp = -999;
                 for (int i = start; i <= end; i++)
                 {
                     // Go over all sensors.
-                    for (int reading : hourlyReadings[i])
+                    for (double reading : hourlyReadings[i])
                     {
                         minTemp = Math.min(minTemp, reading);
                         maxTemp = Math.max(maxTemp, reading);
                     }
-                    int difference = maxTemp - minTemp;
+                    double difference = maxTemp - minTemp;
                     if (difference > maxTempDiff)
                     {
                         maxTempDiff = difference;
@@ -83,7 +89,7 @@ public class Problem2
             }
 
             // Copy all readings to 1D array then sort it to get top and bottom 5.
-            int[] allReadings = new int[NUM_MINUTES * NUM_SENSORS];
+            double[] allReadings = new double[NUM_MINUTES * NUM_SENSORS];
             for (int i = 0; i < NUM_MINUTES; i++)
             {
                 for (int j = 0; j < NUM_SENSORS; j++)
@@ -94,16 +100,16 @@ public class Problem2
             Arrays.sort(allReadings);
 
             // Output hourly report.
-            System.out.printf("----Hour %d----\n");
-            System.out.printf("Largest temperature difference: %d°F, from minute %d to %d\n", maxTempDiff, maxStart, maxEnd);
+            System.out.printf("----Hour %d----\n", h);
+            System.out.printf("Largest temperature difference: %.2f°F, from minute %d to %d\n", maxTempDiff, maxStart, maxEnd);
             System.out.print("5 lowest temperatures recorded: ");
             for (int i = 0; i < 5; i++)
-                System.out.print(allReadings[i] + " ");
+                System.out.printf("%.2f°F ", allReadings[i]);
             System.out.println();
 
             System.out.print("5 highest temperatures recorded: ");
-            for (int i = allReadings.length-1; i >= allReadings.length-5; i++)
-                System.out.print(allReadings[i] + " ");
+            for (int i = allReadings.length-1; i >= allReadings.length-5; i--)
+                System.out.printf("%.2f°F ", allReadings[i]);
             System.out.println();
 
         }
@@ -114,7 +120,7 @@ class Sensor extends Thread
 {
     public int id;
     // Shared space for storing readings from all sensors.
-    public static int[] sensorReadings = new int[8];
+    public static double[] sensorReadings = new double[8];
     public static int MIN_TEMP = -100;
     public static int MAX_TEMP = 70;
 
@@ -125,13 +131,14 @@ class Sensor extends Thread
 
     public void run()
     {
-        int reading = (int)(Math.random() * (MAX_TEMP - MIN_TEMP + 1)) + MIN_TEMP;
+        double reading = (Math.random() * (MAX_TEMP - MIN_TEMP)) + MIN_TEMP;
         storeReading(reading);
     }
 
     // Ensure mutual exclusion using synchronized method.
-    private synchronized void storeReading(int reading)
+    private synchronized void storeReading(double reading)
     {
+        if (Problem2.DEBUG) System.out.println(id + " read " + reading);
         sensorReadings[id] = reading;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ParallelPresents-TemperatureTakings
-Assignment 3 - Sorting the Minotaur's birthday presents, and also designing sensors for a Mars rover for some reason
+# Instructions
+1. Clone the repo with `git pull https://github.com/NicholasR32/MinotaurMindgames`.
+    - Alternatively, simply download `Problem1.java` and `Problem2.java`.
+2. Run each program with `java Problem1.java`, etc.
+3. Results should appear in your terminal. For example, `java Problem2.java` should output:
+
+# Problem 1: Minotaur's Birthday Presents
+
+# Problem 2: Atmospheric Temperature Reading Module
+The main thread acts as the temperature reading module that manages the 8 temperature sensors on the rover. Each Thread is an instance of the `Sensor` class, which has a shared static array `sensorReadings` with slots for readings from each of the sensors. When a thread is run, it generates a random double in [-100,70], then calls synchronized method `storeReading`, which ensures mutual exclusion on the shared array.
+
+Hours and minutes are simulated with nested for loops. Each "minute", 8 new `Sensor` threads are created, ran in order to get and store a temperature reading, and then joined to ensure complete execution. Once the `sensorReadings` array is filled, it is copied to a row in the 2D array `allReadings`, which stores readings from every sensor for every minute.
+
+After every hour, `allReadings` is processed to find the 5 lowest readings, 5 highest readings, and the 10-minute interval of largest temp difference.
+
+Trial runs of the atmospheric temperature reading module tend to almost always yield lowest temperatures near -100, highest temperatures near 70, and a max temperature difference of near 170 over any 10-minute interval. This is expected, since every hour 480 readings are taken, with a range of only 170 possible degrees Fahrenheit.
+
+![](imgs/output.png)
